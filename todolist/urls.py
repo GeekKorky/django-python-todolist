0198545ed679e845b3d0b6c3bbb1d58afdbdc51c
GeekKorky <kaori03252014@gmail.com>
2018-12-11 14:14:45 +0800

    Initial Commit

---
+++ todolist/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.conf.urls import url, include
+
+urlpatterns = [
+    url(r'^$', include('todos.urls')),
+    url(r'^todos/', include('todos.urls')),
+    url(r'^admin/', admin.site.urls)
+]
