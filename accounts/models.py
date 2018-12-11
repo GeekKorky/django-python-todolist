fc7397ded077218e84acbac1cee73130d2d98b5a
GeekKorky <kaori03252014@gmail.com>
2018-12-11 18:32:25 +0800

    Add signals to user profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,24 @@
 from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 class Accounts(models.Model):
-    username = models.OneToOneField(User)
-    firstname = models.CharField(max_length=200)
-    lastname = models.CharField(max_length=200)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
     description = models.CharField(max_length=100, default='')
+    city = models.CharField(max_length=100, default='')
+    website = models.URLField(default='')
+    phone = models.IntegerField(default=0)
     added_at = models.DateTimeField(default=datetime.now, blank=True)
 
     class Meta:
         verbose_name_plural = "Accounts"
 
-    def __str__(self):
-        return self.username
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = Accounts.objects.create(user=kwargs['instance'])
+
+
+post_save.connect(create_profile, sender=User)
