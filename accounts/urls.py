414a7ce9af472a6f25b9f4f58cbd7223c81a3f39
GeekKorky <kaori03252014@gmail.com>
2018-12-11 21:50:22 +0800

    Add edit profile module and template

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,4 +2,8 @@ from django.conf.urls import url
 from . import views
 from django.shortcuts import render
 
-urlpatterns = [url(r'^$', views.index, name='index')]
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^profile/$', views.view_profile, name='view_profile'),
+    url(r'^profile/edit/$', views.edit_profile, name='edit_profile')
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Accounts
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm
 
 
 def index(request):
     return HttpResponse('Home Accounts')
+
+
+def view_profile(request):
+    args = {'user': request.user}
+    return render(request, 'profile.html', args)
+
+
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/accounts/profile')
+    else:
+        form = UserChangeForm(instance=request.user)
+        args = {'form': form}
+        return render(request, 'edit_profile.html', args)
