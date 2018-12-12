9394d38c22cc5367fdd4f3ee68808974d4f60a7c
GeekKorky <kaori03252014@gmail.com>
2018-12-12 14:55:23 +0800

    Rework Middleware LoginRequired and Include Welcome text

--- todos/urls.py
+++ todos/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import url
 from . import views
 from django.contrib.auth.views import login, logout
 
-app_name = 'todo'
-
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^details/(?P<id>\d+)/$', views.details),
