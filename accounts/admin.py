ccf9ef201dcba0bfae6d5db5999446c06e66abbd
GeekKorky <kaori03252014@gmail.com>
2018-12-12 21:29:35 +0800

    Add columns to admin page and define method to change the label of the column

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Accounts
 
-admin.site.register(Accounts)
+
+#adding admin columns to model
+class AccountsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_info', 'city', 'phone', 'website')
+
+    def user_info(self, obj):
+        return obj.description
+
+    user_info.short_description = 'Info'
+
+
+admin.site.register(Accounts, AccountsAdmin)
